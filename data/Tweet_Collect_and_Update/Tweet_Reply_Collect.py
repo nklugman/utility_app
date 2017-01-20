8aaa252f6a41f572cb2da798f902ed0c1a9c5cba
MaxShihBerkeley <chiamaoshih@gmail.com>
2017-01-20 15:23:11 -0800

    1. Adding more .DS_store to git ignore. 2. Working on reply collect 3. Working on reply update 4. Weird bug in reply collect remain unsolved

--- data/Tweet_Collect_and_Update/Tweet_Reply_Collect.py
+++ data/Tweet_Collect_and_Update/Tweet_Reply_Collect.py
@@ -6,6 +6,8 @@
 
 import tweepy
 import pandas as pd  #to store tweets into csv
+import time
+import datetime
 
 consumer_key = 'mtBURqiQRdYdpPmBFo0ebV3cA'
 consumer_secret = '7moNATGly2V2ZTlu2qxHMgjoYMIiiAurDTJ38Co7e2DtW6OX6f'
@@ -22,30 +24,38 @@ twitter_KPLC = 'kenyapower_care'
 api = tweepy.API(auth)
 max_replies = 1000 ##1000 is the maximum when replies collecting
 
-def get_all_replies(screen_name):
-	all_replies = []
-	new_replies = [status for status in tweepy.Cursor(api.search, q=twitter_KPLC).items(max_replies)]
-	all_replies.extend(new_replies)
-	oldest = all_replies[-1].id - 1
-	while len(new_replies) > 0:
-		print ("getting replies before %s" % (oldest))
-		new_replies = [status for status in tweepy.Cursor(api.search, q=twitter_KPLC,max_id=oldest).items(max_replies)]
-		all_replies.extend(new_replies)
-		oldest = all_replies[-1].id - 1
-		print ("...%s tweets downloaded so far" % (len(all_replies)))
-	data=[[obj.user.screen_name, \
-        obj.id_str, \
-        "%s/%s/%s" % (obj.created_at.year, obj.created_at.month, obj.created_at.day), \
-        "%s:%s:%s" % (obj.created_at.hour,obj.created_at.minute, obj.created_at.second), \
-        obj.text.encode("utf8")] for obj in all_replies ]
-	dataframe=pd.DataFrame(data,columns=['screen_name', \
-                                      'tweet_id', \
-                                      'tweet_date', \
-                                      'tweet_time', \
-                                      'tweet'])
-	dataframe.to_csv("%s_all_replies.csv"%(screen_name),index=False)
+def get_all_replies(target):
+    all_replies = []
+    new_replies = [status for status in tweepy.Cursor(api.search, q=target).items(max_replies)]
+    all_replies.extend(new_replies)
+    oldest = all_replies[-1].id - 1
+    while len(new_replies) > 0:
+        time.sleep(10)
+        print("10 seconds time delay start now.")
+        print ("getting replies before %s" % (oldest))
+        new_replies = [status for status in tweepy.Cursor(api.search, q=twitter_KPLC,max_id=oldest).items(max_replies)]
+        all_replies.extend(new_replies)
+        oldest = all_replies[-1].id - 1
+        print ("...%s replies downloaded so far" % (len(all_replies)))
+    time_now = datetime.datetime.now()    
+    print("Update untill %s" %time_now)
+    data=[[obj.user.screen_name, \
+           obj.id_str, \
+           "%s/%s/%s" % (obj.created_at.year, obj.created_at.month, obj.created_at.day), \
+            "%s:%s:%s" % (obj.created_at.hour,obj.created_at.minute, obj.created_at.second), \
+            obj.text.encode("utf8"), \
+            "%s/%s/%s" % (time_now.year, time_now.month, time_now.day), \
+            "%s:%s:%s" % (time_now.hour, time_now.minute, time_now.second)] for obj in all_replies ]
+    dataframe=pd.DataFrame(data,columns=['screen_name', \
+                                         'tweet_id', \
+                                         'reply_date', \
+                                         'reply_time', \
+                                         'reply', \
+                                         'collecting_date', \
+                                         'collecting_time'])
+    dataframe.to_csv("%s_all_replies.csv"%(target),index=False)
 
 	
 if __name__ == '__main__':
-	#pass in the username of the account you want to download
-	get_all_replies(twitter_KPLC)
+    #pass in the username of the account you want to download
+    get_all_replies(twitter_KPLC)
--- data/Tweet_Collect_and_Update/Tweet_Update.py
+++ data/Tweet_Collect_and_Update/Tweet_Update.py
@@ -28,46 +28,51 @@ api = tweepy.API(auth)
 max_count = 200 ##200 is the maximum of count
 
 def get_latest_tweets(screen_name):
-	count=0
-	latest_tweets=[]
-	data=pd.read_csv("%s_tweets.csv"%(screen_name))
-	since_Id=data['tweet_id'].loc[0]
-	try:
-		new_tweets=api.user_timeline(screen_name=screen_name,since_id=since_Id,count=max_count)
-		oldest = new_tweets[-1].id - 1
-	except IndexError:
-		print ("No new tweets")
-		exit()
-	count=len(new_tweets)
-	latest_tweets.extend(new_tweets)
-	print (latest_tweets[0].id)
-	while len(new_tweets) > 0:
-		new_tweets=api.user_timeline(screen_name=screen_name,max_id=oldest,since_id=since_Id,count=max_count)
-		count = count + len(new_tweets)
-		print ("in loop %s tweets downloaded so far"%(len(new_tweets)))
-		latest_tweets.extend(new_tweets)
-		oldest=latest_tweets[-1].id - 1
-	print (latest_tweets[0].id)
-	print ("count: %s" % count)
-	time_now = datetime.datetime.now()
-	print("Now: %s" %time_now)
-	new_data=[[obj.user.screen_name, \
-            obj.id_str, \
-            "%s/%s/%s" % (obj.created_at.year, obj.created_at.month, obj.created_at.day), \
-            "%s:%s:%s" % (obj.created_at.hour,obj.created_at.minute, obj.created_at.second), \
-            obj.text.encode("utf8"), \
-            "%s/%s/%s" % (time_now.year, time_now.month, time_now.day), \
-            "%s:%s:%s" % (time_now.hour, time_now.minute, time_now.second)] for obj in latest_tweets ]
-	dataframe=pd.DataFrame(new_data,columns=['screen_name', \
-                                          'tweet_id', \
-                                          'tweet_date', \
-                                          'tweet_time', \
-                                          'tweet', \
-                                          'collecting_date', \
-                                          'collecting_time'])
-	dataframe=[dataframe,data]
-	dataframe=pd.concat(dataframe)
-	dataframe.to_csv("%s_tweets.csv"%(screen_name),index=False)
+    count=0
+    latest_tweets=[]
+    data=pd.read_csv("%s_tweets.csv"%(screen_name))
+    since_Id=data['tweet_id'].loc[0]
+	
+    try:
+        new_tweets=api.user_timeline(screen_name=screen_name,since_id=since_Id,count=max_count)
+        oldest = new_tweets[-1].id - 1
+    except IndexError:
+        print ("No new tweets")
+        exit()
+    
+    count=len(new_tweets)
+    latest_tweets.extend(new_tweets)
+    print (latest_tweets[0].id)
+    
+    while len(new_tweets) > 0:
+        new_tweets=api.user_timeline(screen_name=screen_name,max_id=oldest,since_id=since_Id,count=max_count)
+        count = count + len(new_tweets)
+        print ("...%s tweets downloaded so far"%(len(latest_tweets)))
+        latest_tweets.extend(new_tweets)
+        oldest=latest_tweets[-1].id - 1
+    
+    print (latest_tweets[0].id)
+    print ("count: %s" % count)
+    time_now = datetime.datetime.now()
+    print("Now: %s" %time_now)
+    
+    new_data=[[obj.user.screen_name, \
+               obj.id_str, \
+               "%s/%s/%s" % (obj.created_at.year, obj.created_at.month, obj.created_at.day), \
+                "%s:%s:%s" % (obj.created_at.hour,obj.created_at.minute, obj.created_at.second), \
+                obj.text.encode("utf8"), \
+                "%s/%s/%s" % (time_now.year, time_now.month, time_now.day), \
+                "%s:%s:%s" % (time_now.hour, time_now.minute, time_now.second)] for obj in latest_tweets ]
+    dataframe=pd.DataFrame(new_data,columns=['screen_name', \
+                                             'tweet_id', \
+                                             'tweet_date', \
+                                             'tweet_time', \
+                                             'tweet', \
+                                             'collecting_date', \
+                                             'collecting_time'])
+    dataframe=[dataframe,data] 
+    dataframe=pd.concat(dataframe)
+    dataframe.to_csv("%s_tweets.csv"%(screen_name),index=False)
 
 if __name__=='__main__':
-	get_latest_tweets(twitter_KPLC)
+    get_latest_tweets(twitter_KPLC)
