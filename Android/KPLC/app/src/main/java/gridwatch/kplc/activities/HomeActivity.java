1e9fc72a2c9ceddcf7f15df3a99cad22b85d4d86
stellagxy <guoxinyi@airbears2-10-142-34-230.airbears2.1918.berkeley.edu>
2017-02-03 13:36:16 -0800

    gitignore

--- Android/KPLC/app/src/main/java/gridwatch/kplc/activities/HomeActivity.java
+++ Android/KPLC/app/src/main/java/gridwatch/kplc/activities/HomeActivity.java
@@ -81,12 +81,7 @@ public class HomeActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.home, menu);
-        return true;
-    }
+
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
