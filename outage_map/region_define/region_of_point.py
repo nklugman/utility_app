6b128fde859788d286c821f300f8e368af63a08d
MaxShihBerkeley <chiamaoshih@gmail.com>
2017-02-03 16:15:53 -0800

    Fix minor bugs

--- outage_map/region_define/region_of_point.py
+++ outage_map/region_define/region_of_point.py
@@ -9,17 +9,15 @@ Created on Thu Feb  2 20:51:15 2017
 import fiona 
 import shapely.geometry
 
-
 kenya_shape = fiona.open("Kenya_sublocations/kenya_sublocations.shp")
 
 def region_check(points_list):
     count = 0
     for point in points_list:
-        this_point = shapely.geometry.Point(point) 
+        this_point = shapely.geometry.Point(point[1],point[0]) ##longtitude goes first
         for shapefile_rec in kenya_shape:
             if(shapely.geometry.asShape(shapefile_rec['geometry']).contains(this_point)):
-                print("Region found")
-                print("Point %s is in region with id =" %(count) ,shapefile_rec['id'], ".")
+                print("Point %s is in Kenya with region id =" %(count) ,shapefile_rec['id'], ".")
                 ##print(shapefile_rec['geometry'])
                 break;
             if(shapefile_rec['id'] == str(len(kenya_shape)-1)):
@@ -28,6 +26,5 @@ def region_check(points_list):
 
 if __name__ == '__main__':
     ##(lat, long) of Nairobi = (1.2921° S, 36.8219° E)
-    ##longtitude goes first
     points_list = [(36.8219, -1.2921),(-1.2920, 36.8218), (500, 500)]
     region_check(points_list);
