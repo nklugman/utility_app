bedc00aa15612b23b56955eb00637626345dd255
pingles94 <arun.pingali@berkeley.edu>
2017-02-15 14:37:26 -0800

    Facebook feed data

---
+++ data/Fb_Collect_and_Update/Comment_Collect.py
@@ -0,0 +1,42 @@
+import facebook
+import requests
+import pandas as pd
+
+j = 50
+def get_data(post):
+    #print(post['created_time'])
+    kplc_post.append(post['comments'])
+    kplc_id.append(post['id'])
+    kplc_time.append(post['created_time'])
+def post_to_csv():
+    #time_now = datetime.datetime.now()
+    data=[]
+    for i in range (0,j):
+        print (kplc_post[i])
+        #data.append([kplc_post[i]])#, kplc_id[i], kplc_time[i]])
+    dataframe=pd.DataFrame(data,columns=['comments'])#, 'id', 'created_time'])
+    dataframe.to_csv("%s_comments.csv"%('comments'),index=False)
+
+access_token = '1838162886420379|1a9ae182f2a68c3a4dd20665bd8395a4'
+# Look at Kenya Power's profile.
+user = 'KenyaPowerLtd'
+
+graph = facebook.GraphAPI(access_token)
+profile = graph.get_object(user)
+posts = graph.get_connections(profile['id'], 'posts')
+kplc_post=[]
+kplc_id=[]
+kplc_time=[]
+
+for i in range (0,j):
+    try:
+        # Perform some action on each post in the collection we receive from
+        # Facebook.
+        [get_data(post=post) for post in posts['data']]
+        # Attempt to make a request to the next page of data, if it exists.
+        posts = requests.get(posts['paging']['next']).json()
+    except KeyError:
+        # When there are no more pages (['paging']['next']), break from the
+        # loop and end the script.
+        break
+post_to_csv()
---
+++ data/Fb_Collect_and_Update/Post_Collect.py
@@ -0,0 +1,41 @@
+import facebook
+import requests
+import pandas as pd
+
+j = 50
+def get_data(post):
+    #print(post['created_time'])
+    kplc_post.append(post['message'])
+    kplc_id.append(post['id'])
+    kplc_time.append(post['created_time'])
+def post_to_csv():
+    #time_now = datetime.datetime.now()
+    data=[]
+    for i in range (0,j):
+        data.append([kplc_post[i], kplc_id[i], kplc_time[i]])
+    dataframe=pd.DataFrame(data,columns=['message', 'id', 'created_time'])
+    dataframe.to_csv("%s_posts.csv"%('posts'),index=False)
+
+access_token = '1838162886420379|1a9ae182f2a68c3a4dd20665bd8395a4'
+# Look at Kenya Power's profile.
+user = 'KenyaPowerLtd'
+
+graph = facebook.GraphAPI(access_token)
+profile = graph.get_object(user)
+posts = graph.get_connections(profile['id'], 'posts')
+kplc_post=[]
+kplc_id=[]
+kplc_time=[]
+
+for i in range (0,j):
+    try:
+        # Perform some action on each post in the collection we receive from
+        # Facebook.
+        [get_data(post=post) for post in posts['data']]
+        # Attempt to make a request to the next page of data, if it exists.
+        posts = requests.get(posts['paging']['next']).json()
+    except KeyError:
+        # When there are no more pages (['paging']['next']), break from the
+        # loop and end the script.
+        break
+post_to_csv()
