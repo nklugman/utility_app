764d4eb5f80ee7803b2a88286eb985eb0287c4f2
MaxShihBerkeley <chiamaoshih@gmail.com>
2017-02-14 18:37:37 -0800

    1.Different outage map for different counties. 2.Smooth the code. Still working on this two.

---
+++ outage_map/Map_Main/map_main.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 10 13:21:00 2017
+
+@author: CHIAMAO_SHIH
+"""
+
+import region_define
+import map_display
+import psycopg2
+import fiona
+import datetime
+
+####area_list = ["Nairobi", "UC Berkeley", "Bamburi", "Mazeras, Coast Region, Kenya"]
+
+##################################
+###Fetching list of outage area###
+##################################
+print("-----Fetching area list from Database...-")
+con = psycopg2.connect(database='capstone', user='capstone', password='capstone', host='141.212.11.206', port='5432')
+con.autocommit = True
+cur = con.cursor()
+cur.execute("SELECT area FROM outages WHERE index=1")
+rows = cur.fetchall()
+area_list =[]
+for row in rows:
+	area_list.append(row[0])
+print("-----Area list fetched.")
+
+##############################################################
+###Find the IDs of sublocations that covers the given area.###
+##############################################################
+print("-----Identifying the locations of each area.")
+id_list = []
+id_list = region_define.region_check(area_list)
+print("-----The IDs of corresponding sublocations found.")
+
+#############################################
+####From id list to polygon on Google Map.###
+#############################################
+print("-----Displaying the outage map through Google Map.")
+kenya_shape = fiona.open("Kenya_sublocations/kenya_sublocations.shp")
+default_blue = '#00008B'
+##mymap = display_logic.GoogleMapPlotter(-1.2921, 36.8219, 16) ##Setting the center of the map displayed.
+mymap = map_display.GoogleMapPlotter.from_geocode("Nairobi") ##Setting the center of the map displayed.
+for point in id_list:
+    mymap.polygon(map_display.get_long_path(point), map_display.get_lat_path(point))
+time_now = datetime.datetime.now()
+mymap.draw('./%s.html' %time_now.date())
+print("-----Map is good to go.")
+
+
---
+++ outage_map/Map_Main/shapefiles_preprocess.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 14 13:35:50 2017
+
+@author: CHIAMAO_SHIH
+"""
+
+import fiona
