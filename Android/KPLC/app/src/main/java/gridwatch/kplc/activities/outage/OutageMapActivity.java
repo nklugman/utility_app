dc86165fdfafc060f59eeaefaa98867f33a5c631
nklugman <nklugman@umich.edu>
2017-02-27 09:39:14 -0500

    adding in webview... will need to add app id and app location posting to webview in the near future

--- Android/KPLC/app/src/main/java/gridwatch/kplc/activities/outage/OutageMapActivity.java
+++ Android/KPLC/app/src/main/java/gridwatch/kplc/activities/outage/OutageMapActivity.java
@@ -2,8 +2,7 @@ package gridwatch.kplc.activities.outage;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
-import gridwatch.kplc.R;
+import android.webkit.WebView;
 
 public class OutageMapActivity extends AppCompatActivity {
 
@@ -12,6 +11,13 @@ public class OutageMapActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_outage_map);
+        WebView webview = new WebView(this);
+        setContentView(webview);
+        webview.loadUrl("http://141.212.11.206/map/map.html");
+
+
+        //setContentView(R.layout.activity_outage_map);
+
+
     }
 }
