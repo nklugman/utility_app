956a900d396efeb7db4a8c201004b3d39315f3d7
MaxShihBerkeley <chiamaoshih@gmail.com>
2017-03-01 12:31:11 -0800

    Clear csv related code

--- data/Tweet_Collect_and_Update/Tweet_Update.py
+++ data/Tweet_Collect_and_Update/Tweet_Update.py
@@ -26,16 +26,17 @@ twitter_KPLC = 'kenyapower_care'
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_key, access_secret)
 api = tweepy.API(auth)
-max_count = 200 ##200 is the maximum of count
+max_count = 200 ##200 is the maximum of count in 'user_timeline'
+tweet_fetch_limit = 3000 ##actually it's some number around 3500
 
-def get_latest_tweets(screen_name):
+def get_latest_tweets(screen_name, since_ID):
     latest_tweets=[]    
     ############################################################
     ####TODO: Get the 'tweet_id' of the newest tweet on database
-    ############################################################
-    since_Id = 834536583705587000
+    ############################################################  
     try:
-        new_tweets=api.user_timeline(screen_name=screen_name,since_id=since_Id,count=max_count)
+        new_tweets=api.user_timeline(screen_name=screen_name,since_id=since_ID,count=max_count)
+        latest_tweets.extend(new_tweets)
         oldest = new_tweets[-1].id - 1
     except IndexError:
         print ("No new tweets")
@@ -43,20 +44,13 @@ def get_latest_tweets(screen_name):
     
     counting = len(new_tweets)
     latest_tweets.extend(new_tweets)
-    print ("...%s tweets downloaded so far"%(len(latest_tweets)))
-    print (latest_tweets[0].id)
     
-    while len(new_tweets) > 0:
-        new_tweets=api.user_timeline(screen_name=screen_name,max_id=oldest,since_id=since_Id,count=max_count)
+    while len(latest_tweets) < tweet_fetch_limit:
+        new_tweets=api.user_timeline(screen_name=screen_name, max_id=oldest, since_id=since_ID,count=max_count)
         counting = counting + len(new_tweets)
         print ("...%s tweets downloaded so far"%(len(latest_tweets)))
         latest_tweets.extend(new_tweets)
         oldest=latest_tweets[-1].id - 1
-    
-    new_tweets=api.user_timeline(screen_name=screen_name,max_id=oldest,since_id=since_Id,count=max_count)
-    counting = counting + len(new_tweets)
-    latest_tweets.extend(new_tweets)
-    oldest=latest_tweets[-1].id - 1
 
     print (latest_tweets[0].id)
     print ("count: %s" % counting)
@@ -64,27 +58,35 @@ def get_latest_tweets(screen_name):
     print("Now: %s" %time_now)
     new_data = []
     for obj in latest_tweets:
-        if(obj.in_reply_to_status_id == ""):
+        if((obj.in_reply_to_status_id == None) & (obj.in_reply_to_screen_name == None)):
             new_data.append([obj.user.screen_name, \
                    obj.id_str, \
                    "%s/%s/%s" % (obj.created_at.year, obj.created_at.month, obj.created_at.day), \
                     "%s:%s:%s" % (obj.created_at.hour,obj.created_at.minute, obj.created_at.second), \
                     obj.text, \
                     "%s/%s/%s" % (time_now.year, time_now.month, time_now.day), \
-                    "%s:%s:%s" % (time_now.hour, time_now.minute, time_now.second)]) 
-
+                    "%s:%s:%s" % (time_now.hour, time_now.minute, time_now.second), \
+                    obj.in_reply_to_status_id, \
+                    obj.in_reply_to_status_id == None, \
+                    obj.in_reply_to_screen_name])
+    since_ID = (latest_tweets[-1].id_str)
     dataframe=pd.DataFrame(new_data,columns=['screen_name', \
                                              'tweet_id', \
                                              'tweet_date', \
                                              'tweet_time', \
                                              'tweet', \
                                              'collecting_date', \
-                                             'collecting_time'])
+                                             'collecting_time', \
+                                             'in_reply_to_status_id', \
+                                             'not_reply',
+                                             'in_reply_to_screen_name'])
     
     try:
         parsing_to_news_feed(dataframe)
     except Exception as e:
         print(e)
+    print(since_ID)
+    return since_ID
 
 def parsing_to_news_feed(tweets):
     con = psycopg2.connect(database='capstone', user='capstone', password='capstone', host='141.212.11.206', port='5432')
@@ -103,4 +105,5 @@ def parsing_to_news_feed(tweets):
 
     
 if __name__=='__main__':
-    get_latest_tweets(twitter_KPLC)
+    sinceID = 833227432403730432
+    get_latest_tweets(twitter_KPLC, sinceID)
